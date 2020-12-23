8bf5b9e9c5abe2d24e228cee7263af62eb2278c9
Nathan Rhead Cox <nrc4@hotmail.com>
2020-12-22 16:34:19 -0800

    Just saving one last time, while it's still working and prior to refactoring.

--- caps.js
+++ caps.js
@@ -1,30 +1,30 @@
 // This is the main hub--the brain--which . . . 
-// (1) manages the state of every package (ready for pickup, in transit, delivered, etc) and 
+// (1) manages the state of every package (ready for pickup, in transit, and delivered) and 
 // (2) logs every event to the console with a timestamp and the event payload, i.e. “EVENT {}”.
 
 'use strict';
 
 const events = require('./events');
 
-require('./modules/driver');
-require('./modules/vendor');
+require('./apps/driver');
+require('./apps/vendor');
 
 events.on('order-received', (payload) => {
   events.emit('pickup', payload);
-  console.log('EVENT', { event: 'pickup', time: new Date(), payload });
+  console.log('EVENT', { event: 'pickup', time: new Date().toString(), payload });
 });
 
 events.on('in-transit', (payload) => {
-  console.log('EVENT', { event: 'in-transit', time: new Date().toString, payload });
+  console.log('EVENT', { event: 'in-transit', time: new Date().toString(), payload });
 });
 
 events.on('delivered', (payload) => {
   events.emit('delivered-message', payload);
-  console.log('EVENT', { event: 'delivered', time: new Date(), payload });
+  console.log('EVENT', { event: 'delivered', time: new Date().toString(), payload });
   console.log('/////////////////////End Transaction///////////////////');
 });
 
-///////////Refactoring per class 12 code reivew//////////
+///////////Refactoring per class 12 code review//////////
 
 // events.on('pickup', (payload) => logEvent('pickup', payload));
 // events.on('in-transit', (payload) => logEvent('in-transit', payload));
