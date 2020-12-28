2be9df3b7d27e6155690dff5d08a5c1c5d4ce897
Nathan Rhead Cox <nrc4@hotmail.com>
2020-12-27 21:09:18 -0800

    Add whatever is done--and it's not much--for the first submission of this lab.

--- apps/drivers/driver.js
+++ apps/drivers/driver.js
@@ -3,27 +3,27 @@
 // const events = require("../../events");
 
 const io = require('socket.io-client');
-const capsVendor = io.connect('http://localhost:3000/caps-namespace');
-// const capsVendor = io.connect(`${host}/caps-namespace`);
+const caps = io.connect('http://localhost:3000/caps-namespace');
+// const caps = io.connect(`${host}/caps-namespace`);
 console.log('Connected to the caps namespace.')
 
 // (1) Monitor the system for events. 
-capsVendor.on('pickup', confirmation);
+caps.on('pickup', confirmation);
 
-// (2) On the ‘pickup’ event, wait 1 second, then log “DRIVER: picked up [ORDER_ID]” to the console.
 function confirmation(payload){
   setTimeout(() => {
-    console.log(`DRIVER: Picked up order No.: ${payload.orderId}.`)
+    // console.log('DRIVER PAYLOAD:', payload)
+    console.log(`DRIVER: Picked up order No.: ${payload.payload.orderId}.`)
 
     // (3) Emit an ‘in-transit’ event with the payload you received.
-    capsVendor.emit('in-transit', payload);
+    caps.emit('in-transit', payload);
   }, 1500);
     
   // (4) Wait 3 seconds, then log “delivered” to the console.
   setTimeout(() => {
-    console.log(`DRIVER: Delivered order No. ${payload.orderId}`);
+    console.log(`DRIVER: Delivered order No. ${payload.payload.orderId}`);
     // (5) Emit a ‘delivered’ event with the same payload.
-    capsVendor.emit('delivered', payload);
+    caps.emit('delivered', payload);
   }, 3000);
 };
 
--- caps.js
+++ caps.js
@@ -8,8 +8,13 @@ const io = require('socket.io')(port);
 
 // Namespaces
 const caps = io.of('/caps-namespace');
+const uuid = require('uuid').v4;
 
 const logEvent = require('./apps/logger');
+
+const queue = {
+  messages: {}
+};
 
 // Turn the socket.io server on.
 io.on('connection', (socket) => {
@@ -17,29 +22,68 @@ io.on('connection', (socket) => {
   });
 
 caps.on('connection', (socket) => {
-  console.log('You are connected to the CAPS namespace', socket.id);
-
+  
   // Meant for direct messaging with the vendor/store.
   socket.on('join', room => { 
     console.log('Joining room #: ', room);
     socket.join(room);
   })
-    // Listen for the vendor socket 'order-received' and emit 'pickup' to the driver.
-  socket.on('pickup', (payload) => { 
+
+  socket.on('pickup', payload => {
     logEvent('pickup', payload);
-    caps.emit('pickup', payload);
+    const messageId = uuid();
+    queue.messages[messageId] = payload; // This is throwing an error; don't understand what it's supposed to be doing
+    socket.emit('added');
+    caps.emit('pickup', { messageId, payload });
   });
+
+  socket.on('in-transit', payload => {
+    logEvent('in-transit', payload);
+    // const messageId = uuid();
+    // queue.messages[messageId] = payload;
+    // socket.emit('added');
+    caps.to(payload.payload.storeName).emit('in-transit', payload);
+    // console.log('Payload.storeName:', payload.payload.storeName);
+  });
+
+  // The payload of the 'received' handler should include the client id, event name, and message id, so that you can delete it from the queue.
+  socket.on('received', payload => {
+    console.log('Heard RECEIVED in the SERVER QUEUE', payload);
+    // console.log('Queue.messages:', queue.messages);
+    delete queue.messages;
+  });
+
+  // The payload should include the client id and event name. 
+  socket.on('get-all', () => {
+    console.log('Listening to GET-ALL in the QUEUE SERVER');
+
+    Object.keys(queue.messages).forEach(id => {
+      socket.emit('message', { id, payload: queue.messages[id]});
+    });
+  });
+
+  socket.on('delivered', payload => {
+    logEvent('delivered', payload);
+    caps.to(payload.payload.storeName).emit('delivered-message', payload);
+    console.log('/////////////////////End Transaction///////////////////');
+  })
+
+    // Listen for the vendor socket 'order-received' and emit 'pickup' to the driver.
+  // socket.on('pickup', (payload) => { 
+  //   logEvent('pickup', payload);
+  //   caps.emit('pickup', payload);
+  // });
 
     // Listen for the driver socket 'in-transit'.
-  socket.on('in-transit', (payload) => {
-    logEvent('in-transit', payload);
-    caps.to(payload.storeName).emit('in-transit', payload);
-  });
+  // socket.on('in-transit', (payload) => {
+  //   logEvent('in-transit', payload);
+  //   caps.to(payload.storeName).emit('in-transit', payload);
+  // });
 
   // Listen for the driver socket 'delivered' and emit 'delivered-message' to the vendor.
-  socket.on('delivered', (payload) => {
-    logEvent('delivered', payload);
-    caps.to(payload.storeName).emit('delivered-message', payload);
-    console.log('/////////////////////End Transaction///////////////////');
-  });
+  // socket.on('delivered', (payload) => {
+    // logEvent('delivered', payload);
+    // caps.to(payload.storeName).emit('delivered-message', payload);
+    // console.log('/////////////////////End Transaction///////////////////');
+  // });
 })
